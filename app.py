635f283e64912f4990ac5ecb5ddd93ab154b6449
guvuriros <163155290+guvuriros@users.noreply.github.com>
2025-02-17 17:00:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
 
 # Generate random time series data
+if st.button("Test this"):
 time_series = np.random.randn(100)
 
 # Plot the time series
