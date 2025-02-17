0e82e9ad755194597a89f021166ddcb7a1ca3282
guvuriros <163155290+guvuriros@users.noreply.github.com>
2025-02-17 17:01:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import streamlit as st
 
 # Generate random time series data
 if st.button("Test this"):
-time_series = np.random.randn(100)
+  time_series = np.random.randn(100)
 
 # Plot the time series
-plt.plot(time_series)
-plt.title("Random 100-Unit Time Series")
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.show()
+  plt.plot(time_series)
+  plt.title("Random 100-Unit Time Series")
+  plt.xlabel("Time")
+  plt.ylabel("Value")
+  plt.show()
